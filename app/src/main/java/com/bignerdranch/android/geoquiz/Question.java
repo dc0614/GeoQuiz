7e87ddb7d872687e9be687802d3005483d6eee03
D Copeland <dcope0614@gmail.com>
2018-06-25 00:05:48 -0400

    Updates to application include adding more than one question, a next button to view and answer the next question and also checks for the correct answer and returns a Toast message displaying whether the user's choice was correct or incorrect.

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+public class Question {
+
+//    Member Variables
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    /*
+        Question Constructor
+     */
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
